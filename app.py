a9f09d5c93462fb3fce544709fc3e11db58acc55
mangrobangit <werlindo.mangrobang@gmail.com>
2019-07-02 15:29:39 -0700

    work on scraping cover images

--- app.py
+++ app.py
@@ -6,10 +6,42 @@ from sklearn.pipeline import Pipeline
 import pickle
 from flask import Flask, request, render_template, jsonify, session, redirect
 
+# Pyspark imports
+import pyspark
+from pyspark.sql import SparkSession
+
+import ast
+
+import comic_recs as cr
+
 # with open('spam_model.pkl', 'rb') as f:
 #     model = pickle.load(f)
 
 app = Flask(__name__, static_url_path="")
+
+# spark config
+spark = SparkSession \
+    .builder \
+    .appName("movie recommendation") \
+    .config("spark.driver.maxResultSize", "1g") \
+    .config("spark.driver.memory", "1g") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.master", "local[*]") \
+    .getOrCreate()
+
+comics_df = spark.read.json('support_data/comics.json')
+comics_df.persist()
+
+comics_sold = spark.read.json('raw_data/als_input_filtered.json')
+comics_sold.persist()
+
+# Create dictionary of candidate parameters
+model_params = {'maxIter': 20
+                 ,'rank': 10
+                 ,'regParam': 0.1
+                 ,'alpha': 40
+                 ,'seed': 41916
+               }
 
 @app.route('/')
 def index():
@@ -36,6 +68,7 @@ def index():
 def predict():
     """Return a prediction of P(spam)."""
     data = request.json
+
     # prediction = model.predict_proba([data['user_input']])
     # round_prediction = round(prediction[0][1], 2)
     # return jsonify({'probability': round_prediction})
@@ -46,9 +79,24 @@ def predict():
 def recommend():
     """Return recomendations."""
     data = request.json
+
+    # Interpret string as literal list
+    reading_list = ast.literal_eval(data['user_input'])
+
+    # Get Recommendations
+    rec_df = cr.make_comic_recommendations(reading_list=reading_list
+                                            ,top_n=10
+                                            ,comics_df=comics_df
+                                            ,train_data=comics_sold
+                                            ,model_params=model_params
+                                            ,spark_instance=spark
+                                            )
+
+    top_rec = rec_df.head(1)['comic_title'].values[0]
+
     # prediction = model.predict_proba([data['user_input']])
     # round_prediction = round(prediction[0][1], 2)
     # return jsonify({'probability': round_prediction})
     # return jsonify({'probability': 'Here are some recommendations.'})
-    return jsonify({'probability': data['user_input']}) 
+    return jsonify({'top_rec': top_rec}) 
     
