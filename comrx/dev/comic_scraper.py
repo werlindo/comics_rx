2df8d406246440a0efc5fdf5712352843db44185
mangrobangit <werlindo.mangrobang@gmail.com>
2019-07-10 07:32:02 -0700

    lint modules

--- comrx/dev/comic_scraper.py
+++ comrx/dev/comic_scraper.py
@@ -1,12 +1,11 @@
-#------------------------------------
+# ------------------------------------
 # Libraries
-#------------------------------------
+# ------------------------------------
 import requests
 import random
 import time
 
 # Selenium
-from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
@@ -14,26 +13,20 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless = True
 
-#------------------------------------
+# ------------------------------------
 # Functions
-#------------------------------------
+# ------------------------------------
+
+
 def scrape_series_covers(browser, titles):
     """Use Selenium to scrape images for comic book titles"""
-    
     start_time = time.time()
-    
+
     for idx, title in enumerate(titles):
         # Search for the title
         search_title(browser, title)
-
-#         title_search_flag = not no_results_found()
-
-#         # Check if no results found
-#         if not title_search_flag:
-#             search_site(browser, title)
-        
         if not no_results_found(browser):
-        # Once on search results, just select first issue of results
+            # Once on search results, just select first issue of results
             click_first_link(browser, title, True)
 
             # Go to the cover gallery of issue page
@@ -48,22 +41,23 @@ def scrape_series_covers(browser, titles):
                       .format(idx, title))
 
                 # Go back to homepage so can do it again!
-                #go_back_home_comicbookdb(browser)
+                # go_back_home_comicbookdb(browser)
         else:
             print("{}.{} was skipped. No title matched.".format(idx, title))
-            
-            
             # Wait random time
             time.sleep(2 + random.random()*5)
-    
-    print ('Total Runtime: {:.2f} seconds'.format(time.time() - start_time))
+
+    print('Total Runtime: {:.2f} seconds'.format(time.time() - start_time))
 
 #     print("All done!")
 
+
 def no_results_found(browser):
+    """Return no result found if path fails"""
     xpath = '/html/body/table/tbody/tr[2]/td[3]'
     result = browser.find_element_by_xpath(xpath)
     return result.text == 'No results found.'
+
 
 def search_title(browser, title):
     """
@@ -72,19 +66,19 @@ def search_title(browser, title):
     """
     # Find search box and enter search text
     text_area = browser.find_element_by_id('form_search')
-    text_area.send_keys(Keys.CONTROL, "a") 
+    text_area.send_keys(Keys.CONTROL, "a")
     text_area.send_keys(title)
-    
+
     # Find Search type dropdown and make sure it says 'Title'
     search_type = Select(browser.find_element_by_name('form_searchtype'))
     search_type.select_by_value('Title')
 
-    
     # Push the search button!
-    sb_xpath = ('/html/body/table/tbody/tr[2]/td[1]' + 
+    sb_xpath = ('/html/body/table/tbody/tr[2]/td[1]' +
                 '/table/tbody/tr[4]/td/form/input[2]')
     search_button = browser.find_element_by_xpath(sb_xpath)
     search_button.click()
+
 
 def search_site(browser, title):
     """
@@ -93,18 +87,16 @@ def search_site(browser, title):
     """
     # Find search box and enter search text
     text_area = browser.find_element_by_id('form_search')
-    text_area.send_keys(Keys.CONTROL, "a") 
+    text_area.send_keys(Keys.CONTROL, "a")
     text_area.send_keys(title)
-    
+
     # Find Search type dropdown and make sure it says 'Title'
-#     search_type = Select(browser.find_element_by_name('form_searchtype'))
-#     search_type.select_by_value('Title')
-    
     # Push the search button!
-    sb_xpath = ('/html/body/table/tbody/tr[2]/td[1]' + 
+    sb_xpath = ('/html/body/table/tbody/tr[2]/td[1]' +
                 '/table/tbody/tr[4]/td/form/input[2]')
     search_button = browser.find_element_by_xpath(sb_xpath)
     search_button.click()
+
 
 def click_first_link(browser, title, title_search_flag):
     """
@@ -113,45 +105,47 @@ def click_first_link(browser, title, title_search_flag):
     # Find first issue link in search results
     if title_search_flag:
         x_path = '/html/body/table/tbody/tr[2]/td[3]/a[1]'
-    else: 
+    else:
         x_path = '/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr/td/a[1]'
-        
-    #first_issue_link = browser.find_element_by_partial_link_text(title)
+
     first_issue_link = browser.find_element_by_xpath(x_path)
-    
+
     # Click
     first_issue_link.click()
+
 
 def go_cover_gallery(browser):
     """
     Click on Cover Gallery button
     """
-    gb_xpath = ("/html/body/table/tbody/tr[2]/td[3]/table[1]" + 
+    gb_xpath = ("/html/body/table/tbody/tr[2]/td[3]/table[1]" +
                 "/tbody/tr/td/a[4]/img"
-               )
-    gb_xpath='//a[img/@src="graphics/button_title_covergallery.gif"]'
-    
+                )
+    gb_xpath = '//a[img/@src="graphics/button_title_covergallery.gif"]'
+
     gallery_btn = browser.find_element_by_xpath(gb_xpath)
     gallery_btn.click()
-    
+
+
 def get_first_image(browser, title):
     """
     Find first image in cover gallery and scrape it!
     """
     # Find first image
-    first_img_path = ('/html/body/table/tbody/tr[2]/td[3]/' + 
+    first_img_path = ('/html/body/table/tbody/tr[2]/td[3]/' +
                       'table/tbody/tr[1]/td[1]/a/img')
-    first_img = browser.find_element_by_xpath(first_img_path)    
+    first_img = browser.find_element_by_xpath(first_img_path)
 
     # Construct path and file name
-    filename = ('./raw_data/covers/' + title.replace(' ', '_').lower() 
-               + '.jpg'
-               )
-    
-    # Save the file in the file/path 
+    filename = ('./raw_data/covers/' + title.replace(' ', '_').lower()
+                + '.jpg'
+                )
+
+    # Save the file in the file/path
     scrape_image(first_img, filename)
-    
+
     return
+
 
 def scrape_image(img, filename):
     """Save an image element as filename"""
@@ -159,12 +153,14 @@ def scrape_image(img, filename):
     img_data = response.content
     with open(filename, 'wb') as f:
         f.write(img_data)
-        
+
+
 def go_back_home_comicbookdb(browser):
+    """Go directly back to comicbookdb.com home via logolink"""
     # Find image link to go back home
-    home_pg_xpath = '/html/body/table/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/a/img'
+    home_pg_xpath = ('/html/body/table/tbody/tr[1]/td/table/tbody' +
+                     '/tr[1]/td/table/tbody/tr/td[1]/a/img')
     logo_btn = browser.find_element_by_xpath(home_pg_xpath)
 
     # Click!
-    logo_btn.click()    
-    
+    logo_btn.click()
--- comrx/dev/data_fcns.py
+++ comrx/dev/data_fcns.py
@@ -1,13 +1,15 @@
-#-------------------------------------
+# -------------------------------------
 # Libraries
-#------------------------------------
+# ------------------------------------
 import pandas as pd
 
-#-------------------------------------
+# -------------------------------------
 # Functions
-#------------------------------------
+# ------------------------------------
 # Original source and credit:
 # https://stackoverflow.com/questions/29882573/pandas-slow-date-conversion
+
+
 def date_converter(s):
     """
     This is an extremely fast approach to datetime parsing.
@@ -15,48 +17,52 @@ def date_converter(s):
     re-parse these, we store all unique dates, parse them, and
     use a lookup to convert all dates.
     """
-    dates = {date:pd.to_datetime(date) for date in s.unique()}
+    dates = {date: pd.to_datetime(date) for date in s.unique()}
     return s.map(dates)
 
+
 def cut_issue_num(title_and_num):
-    """Remove #-sign and all characters to the right.
-    
+    """
+    Remove #-sign and all characters to the right.
     Parameters:
     -----------
     title_and_num = string representing title, issue num and any other details.
     """
     return title_and_num[:title_and_num.rfind('#')].strip()
 
+
 def make_int(curr_str):
     """Take a string and make it an int. No error trapping at this time."""
     return int(curr_str)
 
-#-------------------------------------
+# -------------------------------------
 # References
-#------------------------------------
+# ------------------------------------
+
+
 # Dictionary to map to shorter publisher names.
 pub_dict = {'Amaze Ink Slave Labor Graphics': 'SLG',
-             'Archie Comics': 'Archie',
-             'Aspen MLT': 'Aspen',
-             'Avatar Press': 'Avatar',
-             'Bongo Comics': 'Bongo',
-             'Boom! Studios': 'Boom',
-             'D.D.P.': 'DDP',
-             'D.E.': 'DE',
-             'Dark Horse': 'Dark Horse',
-             'DC Comics': 'DC',
-             'DC Vertigo': 'Vertigo',
-             'DC Wildstorm': 'Wildstorm',
-             'Drawn & Quarterly': 'D&Q',
-             'Fantagraphics': 'Fantagraphics',
-             'IDW Publishing': 'IDW',
-             'Image Comics': 'Image',
-             'Image Topcow': 'Topcow',
-             'Marvel Comics': 'Marvel',
-             'Oni Press': 'Oni',
-             'Other': 'Other',
-             'Radical Publishing': 'Radical',
-             'Red 5 Comics': 'Red 5',
-             'Top Shelf Productions': 'Top Shelf',
-             'Zenescope Entertainment': 'Zenescope'
-           }
+            'Archie Comics': 'Archie',
+            'Aspen MLT': 'Aspen',
+            'Avatar Press': 'Avatar',
+            'Bongo Comics': 'Bongo',
+            'Boom! Studios': 'Boom',
+            'D.D.P.': 'DDP',
+            'D.E.': 'DE',
+            'Dark Horse': 'Dark Horse',
+            'DC Comics': 'DC',
+            'DC Vertigo': 'Vertigo',
+            'DC Wildstorm': 'Wildstorm',
+            'Drawn & Quarterly': 'D&Q',
+            'Fantagraphics': 'Fantagraphics',
+            'IDW Publishing': 'IDW',
+            'Image Comics': 'Image',
+            'Image Topcow': 'Topcow',
+            'Marvel Comics': 'Marvel',
+            'Oni Press': 'Oni',
+            'Other': 'Other',
+            'Radical Publishing': 'Radical',
+            'Red 5 Comics': 'Red 5',
+            'Top Shelf Productions': 'Top Shelf',
+            'Zenescope Entertainment': 'Zenescope'
+            }
